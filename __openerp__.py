c08fb786d3163598c66359877667293e5ee7f4d1
Marcel van der Boom <marcel@hsdev.com>
2013-09-24 17:11:44 +0200

    Add default types as system data

--- __openerp__.py
+++ __openerp__.py
@@ -3,20 +3,21 @@
 #
 # Meta information about the substances module
 {
-	"name"       : "Phrase management",
-	"version"    : "1.0",
-	"author"     : "HSD",
-	"category"   : "Generic Modules/Others",
-	"website"    : "http://hsdev.com",
-	"url"        : "http://hsdev.com",
-        "summary"    : "Manage legislative product phrases",
-	"description": "Module to manage phrases/statement for products (examples: R/S phrases, Health statements etc.)",
-	"depends"    : ["base","product","stock"],
-	"init_xml"   : [],
-	"update_xml" : [
-	  "phrase_view.xml",
-	  'security/ir.model.access.csv'
+	'name'       : 'Phrase management',
+	'version'    : '1.0',
+	'author'     : 'HSD',
+	'category'   : 'Generic Modules/Others',
+	'website'    : 'http://hsdev.com',
+	'url'        : 'http://hsdev.com',
+        'summary'    : 'Manage legislative product phrases',
+	'description': 'Module to manage phrases/statement for products (examples: R/S phrases, Health statements etc.)',
+	'depends'    : ['base','product','stock'],
+	'init_xml'   : [],
+	'update_xml' : [
+	  'phrase_view.xml',
+	  'security/ir.model.access.csv',
+          'data/product.phrase.type.csv'
 	],
-	"active"     : False,
-	"installable": True
+	'active'     : False,
+	'installable': True
 }
